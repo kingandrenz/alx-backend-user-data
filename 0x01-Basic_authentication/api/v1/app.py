18f73dbcd417edada430179a916e665f6e6f4f42
kanu Anthony <kanuchibueze@gmail.com>
2023-08-09 00:53:43 +0000

    task1

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -9,11 +9,6 @@ from flask_cors import (CORS, cross_origin)
 import os
 
 
-from api.v1.views import app_views
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
-
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
