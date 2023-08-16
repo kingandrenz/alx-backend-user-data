7663b23311d965d5cd0367f6fe9385db49601989
kanu Anthony <kanuchibueze@gmail.com>
2023-08-16 19:25:28 +0000

    updated user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python3
-""" SQLAlchemy User model
-"""
-
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
